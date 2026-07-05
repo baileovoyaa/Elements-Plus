cdf0145f309b5c47b1ded6b5e0860ccd1853804d
myworldzycpc <myworldzhangyichen@qq.com>
2026-07-05 20:56:22 +0800

    结晶器配方书位置调整

--- src/client/java/com/elementsplus/client/screen/CrystallizerScreen.java
+++ src/client/java/com/elementsplus/client/screen/CrystallizerScreen.java
@@ -3,11 +3,14 @@ package com.elementsplus.client.screen;
 import com.elementsplus.menu.CrystallizerMenu;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.AbstractFurnaceScreen;
+import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.client.gui.screens.recipebook.SmeltingRecipeBookComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.RecipeBookMenu;
 
 import static com.elementsplus.ElementsPlus.MOD_ID;
 
@@ -19,5 +22,20 @@ public class CrystallizerScreen extends AbstractFurnaceScreen<CrystallizerMenu> 
 
     public CrystallizerScreen(CrystallizerMenu furnaceMenu, Inventory inventory, Component component) {
         super(furnaceMenu, new SmeltingRecipeBookComponent(), inventory, component, TEXTURE, LIT_PROGRESS_SPRITE, BURN_PROGRESS_SPRITE);
+    }
+
+    @Override
+    public void init() {
+        this.leftPos = (this.width - this.imageWidth) / 2;
+        this.topPos = (this.height - this.imageHeight) / 2;
+        boolean widthTooNarrow = this.width < 379;
+        this.recipeBookComponent.init(this.width, this.height, this.minecraft, widthTooNarrow, this.menu);
+        this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
+        this.addRenderableWidget(new ImageButton(this.leftPos + 10, this.height / 2 - 49, 20, 18, RecipeBookComponent.RECIPE_BUTTON_SPRITES, button -> {
+            this.recipeBookComponent.toggleVisibility();
+            this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
+            button.setPosition(this.leftPos + 20, this.height / 2 - 49);
+        }));
+        this.titleLabelX = (this.imageWidth - this.font.width(this.title)) / 2;
     }
 }
